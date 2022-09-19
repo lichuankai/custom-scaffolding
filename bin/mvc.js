6fee483106d6f4f40907170e9ec356897853f5c5
lixiaobai <1054191102@qq.com>
2022-09-20 00:21:51 +0800

    feat: 添加脚手架

---
+++ bin/mvc.js
@@ -0,0 +1,12 @@
+import { program } from 'commander';
+import create from '../lib/create.js';
+
+program
+  .version('0.1.0')
+  .command('create <name>')
+  .description('create a new project')
+  .action(name => {
+    create(name); 
+  })
+ 
+program.parse();
